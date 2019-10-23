d08da7849e1f56df29ae2a32cfc8a816b51cbf4c
Mathieu Durand <mathieu.durand@gmail.com>
2019-10-22 21:55:26 -0400

    feat: latest version with secret file

--- apply.sh
+++ apply.sh
@@ -1,11 +1,17 @@
-SECRETS=$1/secrets.enc.env
-DECRYPTED_SECRETS=$1/secrets.env
-if test -f "$SECRETS"; then
-    sops -d $SECRETS > $DECRYPTED_SECRETS
-fi
+#!/bin/bash
 
+#Decrypt secrets
+for i in $1/*.enc.*; do
+    decryptedFile="${i/.enc/}"
+    echo "Decrypting $i >> $decryptedFile"
+    sops -d $i > $decryptedFile
+done
+
+kustomize build $1
 kustomize build $1 | kubectl apply -f -
 
-if test -f "$DECRYPTED_SECRETS"; then
-    rm -f $DECRYPTED_SECRETS
-fi
+#Remove decrypted files
+for i in $1/*.enc.*; do
+    decryptedFile="${i/.enc/}"
+    rm -f $decryptedFile
+done
